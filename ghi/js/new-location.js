b443472f8d1b1744ed2913d7a8f2052c18197673
Phat <ptnguyen728@gmail.com>
2023-07-13 17:09:22 -0500

    create a location

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,1 +1,41 @@
-console.log("New Location js loaded")
+
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const url = 'http://localhost:8000/api/states/';
+  console.log("DOM for New Location js loaded")
+
+
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    const selectTag = document.getElementById('state')
+
+    for (let state of data.states) {
+      const optionElement = document.createElement('option');
+      optionElement.value = state.abbreviation;
+      optionElement.innerHTML = state.name;
+      selectTag.appendChild(optionElement);
+    }
+  }
+
+  const formTag = document.getElementById('create-location-form');
+  formTag.addEventListener('submit', async event => {
+    event.preventDefault();
+    const formData = new FormData(formTag);
+    const json = JSON.stringify(Object.fromEntries(formData));
+
+    const locationUrl = 'http://localhost:8000/api/locations/';
+    const fetchConfig = {
+      method: "post",
+      body: json,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(locationUrl, fetchConfig);
+    if (response.ok) {
+      formTag.reset();
+      const newLocation = await response.json();
+    }
+  });
+});
