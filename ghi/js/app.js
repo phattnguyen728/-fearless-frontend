23d7d55792aeb85fb9ad292cf247951aebf345a9
Phat <ptnguyen728@gmail.com>
2023-07-12 23:05:31 -0500

    card template complete

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,77 +1,60 @@
-// window.addEventListener('DOMContentLoaded', async () => {
-//   const url = 'http://localhost:8000/api/conferences/';
-//   const response = await fetch(url);
-//   console.log(response);
-
-//   const data = await response.json();
-//   console.log(data);
-
-// });
-
-
-// window.addEventListener('DOMContentLoaded', async () => {
-//   const url = 'http://localhost:8000/api/conferences/';
-
-//   try {
-//     const response = await fetch(url);
-
-//     if (!response.ok) {
-//       // Figure out what to do when the response is bad
-//       throw new Error('Response not ok');
-//     } else {
-//       const data = await response.json();
-     //  const conference = data.conferences[0];
-     //  console.log(conference);
-     //  const nameTag = document.querySelector('.card-title');
-      // nameTag.innerHTML = conference.name;
-
-      // const detailUrl = `http://localhost:8000${conference.href}`;
-      // const detailResponse = await fetch(detailUrl);
-      // if (detailResponse.ok) {
-        // const details = await detailResponse.json();
-        // console.log(details);
-      // }
-
-//     }
-//   } catch (e) {
-//     console.error('error', e)
-//     // Figure out what to do if an error is raised
-//   }
-
-// });
-
-
-// my code makes better sense, but their code is above
 window.addEventListener('DOMContentLoaded', async () => {
+
+  function createCard(name, description, pictureUrl, startDate, endDate, locationName) {
+    return `
+    <div class="col-md-4">
+      <div class="shadow-lg p-3 mb-5 bg-white rounded">
+        <div class="card">
+          <img src="${pictureUrl}" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${locationName}</h6>
+            <p class="card-text">${description}</p>
+            <div class="card-footer">${startDate} - ${endDate}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
   const url = 'http://localhost:8000/api/conferences/';
 
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      const conference = data.conferences[0];
-      // console.log(conference);
-      const nameTag = document.querySelector('.card-title');
-      nameTag.innerHTML = conference.name;
+      const column = document.querySelector('.row')
 
-      const detailUrl = `http://localhost:8000${conference.href}`;
-      const detailResponse = await fetch(detailUrl);
-      if (detailResponse.ok) {
-        const details = await detailResponse.json();
-        console.log(details);
-        const descriptionTag = document.querySelector('.card-text');
-        descriptionTag.innerHTML = details.conference.description;
-
-        // console.log(details.conference.picture_url)
-        const imgTag = document.querySelector('.card-img-top');
-        imgTag.src = details.conference.location.picture_url;
-
+      for (let conference of data.conferences) {
+        const detailUrl = `http://localhost:8000${conference.href}`;
+        const detailResponse = await fetch(detailUrl);
+        if (detailResponse.ok) {
+          const details = await detailResponse.json();
+          const title = details.conference.name;
+          const description = details.conference.description;
+          const pictureUrl = details.conference.location.picture_url;
+          const startDate = new Date(details.conference.starts).toLocaleDateString();
+          const endDate = new Date(details.conference.ends).toLocaleDateString();
+          const locationName = details.conference.location.name
+          const html = createCard(title, description, pictureUrl, startDate, endDate, locationName);
+          const div = document.createElement('div');
+          div.innerHTML = html.trim();
+          column.appendChild(div.firstChild);
+        }
       }
-
     } else {
       throw new Error('Response not ok');
     }
   } catch (e) {
-    console.error('error', e);
+    console.error(e);
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'alert alert-danger alert-dismissible fade show';
+    errorDiv.innerHTML = `
+      <strong>Error!</strong> ${e}.
+      <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    `;
+    const container = document.querySelector('.container');
+    container.insertBefore(errorDiv, container.firstChild);
   }
 });
--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -98,30 +98,6 @@ def api_list_conferences(request):
 
 @require_http_methods(["GET", "PUT", "DELETE"])
 def api_show_conference(request, pk):
-    """
-    Returns the details for the Conference model specified
-    by the pk parameter.
-
-    This should return a dictionary with the name, starts,
-    ends, description, created, updated, max_presentations,
-    max_attendees, and a dictionary for the location containing
-    its name and href.
-
-    {
-        "name": the conference's name,
-        "starts": the date/time when the conference starts,
-        "ends": the date/time when the conference ends,
-        "description": the description of the conference,
-        "created": the date/time when the record was created,
-        "updated": the date/time when the record was updated,
-        "max_presentations": the maximum number of presentations,
-        "max_attendees": the maximum number of attendees,
-        "location": {
-            "name": the name of the location,
-            "href": the URL for the location,
-        }
-    }
-    """
     if request.method == "GET":
         conference = Conference.objects.get(id=pk)
         weather = get_weather_data(
@@ -159,24 +135,6 @@ def api_show_conference(request, pk):
 
 @require_http_methods(["GET", "POST"])
 def api_list_locations(request):
-    """
-    Lists the location names and the link to the location.
-
-    Returns a dictionary with a single key "locations" which
-    is a list of location names and URLS. Each entry in the list
-    is a dictionary that contains the name of the location and
-    the link to the location's information.
-
-    {
-        "locations": [
-            {
-                "name": location's name,
-                "href": URL to the location,
-            },
-            ...
-        ]
-    }
-    """
     if request.method == "GET":
         locations = Location.objects.all()
         return JsonResponse(
