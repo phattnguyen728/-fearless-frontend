2de2a488e5b7ac76583a469168b2b6b3d78a82f3
Phat <ptnguyen728@gmail.com>
2023-07-14 01:06:49 -0500

    Sign up another form added

---
+++ ghi/js/attend-conference.js
@@ -0,0 +1,60 @@
+window.addEventListener('DOMContentLoaded', async () => {
+  const selectTag = document.getElementById('conference');
+
+  const url = 'http://localhost:8000/api/conferences/';
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+
+    for (let conference of data.conferences) {
+      const option = document.createElement('option');
+      option.value = conference.href;
+      option.innerHTML = conference.name;
+      selectTag.appendChild(option);
+    }
+    // selectTag.classList.add('d-none');
+    selectTag.classList.remove('d-none')
+  }
+
+});
+
+
+// window.addEventListener('DOMContentLoaded', async () => {
+//   const url = 'http://localhost:8000/api/conferences/';
+//   const response = await fetch(url);
+//   const selectTag = document.getElementById("conference");
+
+//   if (response.ok) {
+//     const data = await response.json();
+
+//     for (let conference of data.conferences) {
+//       const optionElement = document.createElement("option");
+//       optionElement.value = conference.id;
+//       optionElement.innerHTML = conference.name;
+//       selectTag.appendChild(optionElement);
+//     }
+//   }
+
+//   const formTag = document.getElementById("create-presentation-form")
+//   formTag.addEventListener("submit", async (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(formTag);
+//     const json = JSON.stringify(Object.fromEntries(formData));
+//     const conf_id = selectTag.options[selectTag.selectedIndex].value
+
+//     const presentationUrl = `http://localhost:8000/api/conferences/${conf_id}/presentations/`;
+//     const fetchConfig = {
+//       method: "post",
+//       body: json,
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     };
+
+//     const response = await fetch(presentationUrl, fetchConfig);
+//     if (response.ok) {
+//       formTag.reset();
+//       const newPresentation = await response.json
+//     }
+//   });
+// });
