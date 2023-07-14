0d7de6f8cbfbf62a252cdaa53d29c60c5ecc7ab5
Phat <ptnguyen728@gmail.com>
2023-07-14 02:41:11 -0500

    completed adding attendees

--- attendees_microservice/attendees_bc/settings.py
+++ attendees_microservice/attendees_bc/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "attendees.apps.AttendeesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
+    "corsheaders",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
@@ -45,12 +46,18 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8001",
 ]
 
 CRONJOBS = [
--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded', async () => {
   const selectTag = document.getElementById('conference');
+  const successMessage = document.getElementById('success-message');
+  const loadIcon = document.getElementById("loading-conference-spinner");
+
 
   const url = 'http://localhost:8000/api/conferences/';
   const response = await fetch(url);
@@ -12,49 +15,31 @@ window.addEventListener('DOMContentLoaded', async () => {
       option.innerHTML = conference.name;
       selectTag.appendChild(option);
     }
-    // selectTag.classList.add('d-none');
+
+    loadIcon.classList.add('d-none')
     selectTag.classList.remove('d-none')
   }
 
+  const formTag = document.getElementById("create-attendee-form")
+  formTag.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(formTag);
+    const json = JSON.stringify(Object.fromEntries(formData));
+
+    const attendeesUrl = `http://localhost:8001/api/attendees/`;
+    const fetchConfig = {
+      method: "post",
+      body: json,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(attendeesUrl, fetchConfig);
+    if (response.ok) {
+      formTag.classList.add('d-none');
+      successMessage.classList.remove('d-none');
+      formTag.reset();
+      const newAttendee = await response.json()
+    }
+  });
 });
-
-
-// window.addEventListener('DOMContentLoaded', async () => {
-//   const url = 'http://localhost:8000/api/conferences/';
-//   const response = await fetch(url);
-//   const selectTag = document.getElementById("conference");
-
-//   if (response.ok) {
-//     const data = await response.json();
-
-//     for (let conference of data.conferences) {
-//       const optionElement = document.createElement("option");
-//       optionElement.value = conference.id;
-//       optionElement.innerHTML = conference.name;
-//       selectTag.appendChild(optionElement);
-//     }
-//   }
-
-//   const formTag = document.getElementById("create-presentation-form")
-//   formTag.addEventListener("submit", async (event) => {
-//     event.preventDefault();
-//     const formData = new FormData(formTag);
-//     const json = JSON.stringify(Object.fromEntries(formData));
-//     const conf_id = selectTag.options[selectTag.selectedIndex].value
-
-//     const presentationUrl = `http://localhost:8000/api/conferences/${conf_id}/presentations/`;
-//     const fetchConfig = {
-//       method: "post",
-//       body: json,
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     };
-
-//     const response = await fetch(presentationUrl, fetchConfig);
-//     if (response.ok) {
-//       formTag.reset();
-//       const newPresentation = await response.json
-//     }
-//   });
-// });
